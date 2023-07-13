a88fd29fa3c4885ed11d9c6d21cef9436f73f2ba
lwlR <47115057+lwlR@users.noreply.github.com>
2023-07-13 11:09:51 +0200

    terra uebung aus "general.R" geloescht

--- general.R
+++ general.R
@@ -31,30 +31,11 @@ library(lidR)
 #----------------------------------------------------------------------
 # morning part
 #----------------------------------------------------------------------
+# ------> fuer terra siehe Datei terra.R
+
 
 
 maindir = 'C:/Users/paule/Desktop/p6/'
-# 1. Terra
-# - spielen Multispektral
-# - shapefile aus den plots erstellen
-# Read orthophoto in R
-orth <- rast(file.path(maindir, "20230707_EDNX-ortho.tif"))
-# Get summary of file
-orth
-# Create a first plot
-plotRGB(orth)
-# Vector data
-# Load table with field measurements
-fd <- read.csv("fielddata.csv")#
-# Create shapefile from field measurements
-fd_shp <- vect(fd, geom=c("lon", "lat"), crs="", keepgeom=FALSE) # Needs to be updated with the correct columns names and crs
-# Plot field data above orthophoto
-plotRGB(orth)
-plot(fd_shp, add = TRUE, col = "darkred")
-# Save shapefile to disc
-writeVector(fd_shp, file.path(maindir, "20230707_EDNX_fieldData.shp")
-
-
 
 # 2. lidR
 las04 = readLAS(file.path(maindir, "20230422_EDNX_viehtraenke.laz"))
