ec5ce391df89c4bab07e673c47c4f825a776742f
Woodchuck <ben.hartweg@campus.lmu.de>
2023-07-19 13:50:39 +0200

    to show git

--- biomass.R
+++ biomass.R
@@ -1,1 +1,20 @@
+install.packages("httpgd")
+
+library(lidR)
+library(terra)
 #biomass algorithm for group 1
+
+#local repo
+
+path = "D:/biomass"
+
+
+#read the shapes
+points  = terra::vect(file.path(path, "field_measuremnts.shp"))
+shp_east = terra::vect(file.path(path, "ext_plot_east.shp"))
+
+#read the ortho
+dop_east = rast(file.path(path, "20230707_EDNX_ortho_ext_plot_east.tif"))
+
+window()
+plotRGB(dop_east)
